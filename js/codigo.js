dbffc4f3620ed61d0596781e8c1da6ce4ad3917e
Marcoslarraza <markkusito@gmail.com>
2022-05-29 02:10:27 -0300

    algoritmo bucle

---
+++ js/codigo.js
@@ -0,0 +1,9 @@
+
+let color = prompt("Ingresa un color. /n Para salir ingresa rojo");
+while (color != "rojo") {
+    console.log ("El color elejido es " + color);
+
+    color= prompt("Ingresa un color. /n Para salir ingresa rojo")
+}
+
+
