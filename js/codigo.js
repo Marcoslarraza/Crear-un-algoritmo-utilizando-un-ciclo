6c758782be302ba6abc9448a5841eb5b8dc82c1f
Marcoslarraza <markkusito@gmail.com>
2022-06-02 23:17:46 -0300

    desafio clase 4 algoritmo iva

--- js/codigo.js
+++ js/codigo.js
@@ -1,9 +1,26 @@
-
-let color = prompt("Ingresa un color. /n Para salir ingresa rojo");
-while (color != "rojo") {
-    console.log ("El color elejido es " + color);
-
-    color= prompt("Ingresa un color. /n Para salir ingresa rojo")
-}
+ 
 
 
+let productoIngresado = prompt("Ingresa tu producto")
+
+let precioProducto = Number(prompt("Ingresa el precio"))
+
+//la palabra number permite transformar a un numero real
+//como ejemplo siempre es mejor crear la variable de manera global por fuera de la función
+//como es el caso de "let iva" seria mejor usarla por fuera
+
+
+function precioTotal(productoIngresado, precioProducto) {
+
+    let iva = 21
+    
+    let ivaCalculado = precioProducto * iva / 100
+
+    let final = ivaCalculado + precioProducto
+
+    alert ("el precio final de " + productoIngresado + " con iva es: " + final)
+
+} 
+precioTotal(productoIngresado, precioProducto)
+
+//cuando llamo a la función debo agregar los parametros que usé al crearla  */
---
+++ js/prueba.js
@@ -0,0 +1,11 @@
+ function saludar() {
+    respuesta = prompt("¡Hola Marcos! ¿Como fue tu día?");
+    if (respuesta === "bien") {
+        alert("Me re alegro!!")
+    } else {
+        alert("Uh, que mal")
+    }
+}
+
+saludar()  
+
